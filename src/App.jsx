1dbf1f2d9d32abb89f0fb7ce5ffe54fff9fe43a6
anupa kushwha <anupk6347@gmail.com>
2024-09-10 23:16:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+    <div className='bg-black text-3xl  text-white' >
+      anupam 
+    </div>
+    </>
+  )
+}
+
+export default App
